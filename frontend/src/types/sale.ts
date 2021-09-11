d25d6c0ba350915fdd3232f32405052362553a6b
Tiago Sampaiogit config --global user.name Tiago <teagolos10@gmail.com>
2021-09-10 21:24:05 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,10 +1,34 @@
-export type SaleSum = {
-    sellerName: string;
-    sum: number;
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+    
 }
 
-export type SaleSuccess = {
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    first: boolean;
+    number: number;
+    numberOfElements?: number;
+    size?: number;
+    empty?: boolean;
+}
+
+export type SaleSum = {
         sellerName: string;
-        visited: number;
-        deals: number;
+        sum: number;
+    }
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
